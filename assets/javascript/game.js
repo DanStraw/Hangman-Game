be2049db4e860d101c5a26fdc0ce3379123c794f
Daniel <danstraw86@gmail.com>
2018-02-12 18:17:20 -0800

    added pseudocode to javascript file. Fine-tuned layout in html and css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,17 @@
-console.log('test');
+//create a variable called characters. Will be an array of of character names. 
+
+//create variable computerChoice. This will randomly select one of the values from the characters var (Math.random)
+
+//have word blank on game board populate with number of underscores equal to the number of letters in the computerChoice word
+
+//computerChoice will occur on page load and when user hits enter after each game is completed
+
+//use onKeyUp to determine userChoice
+
+//compare userChoice to the letters in computerChoice. If there is no match, add 1 to incorrect guesses
+
+//if there is a match between userChoice and computerChoice, have said letter replace the underscore in the word blank
+
+//if incorrect guesses equals 8 before all correct letters are guessed, user loses.
+
+//if all correct letters are guessed before 8 incorrect guesses, user wins. Display picture of character alongside name.
