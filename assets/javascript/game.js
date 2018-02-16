f33ea707348f44f7b6ef0554c51d9ce8f896ecf3
Daniel <danstraw86@gmail.com>
2018-02-16 00:29:59 -0800

    added starscape photo as background to body. Expanded words array.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,9 @@
 
 window.onload = function() {
     //create a variable called words. Will be an array of names of bodies in the solar system
-    var words = ["Sun", "Earth", "Moon", "Mercury", "Venus", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"];
+    var words = ["Sun", "Earth", "Moon", "Mercury", "Venus", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto", 'Ganymede', 'Phobos',
+        'Deimos', 'Ceres', 'Haumea', 'Makemake', 'Eris', 'Callisto', 'Europa', 'Hegemone', 'Io', 'Thebe', 'Atlas', 'Calypso', 'Hyperion', 'Janus',
+        'Pandora', 'Phoebe', 'Titan', 'Ariel', 'Desdemona', 'Galatea', 'Triton', 'Hydra', 'Styx'];
     var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     var wins = 0;
     var losses = 0;
